5c3963cd950d91abef485d4d65fbfb21f115be9b
blissLee2 <150557827+blissLee2@users.noreply.github.com>
2025-05-11 23:50:46 +0200

    Add files via upload
    updated EJB

---
+++ HomeworkEJB/HomeworkEJB/build/generated-sources/ap-source-output/entities/Complaint_.java
@@ -0,0 +1,16 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-01T20:26:30")
+@StaticMetamodel(Complaint.class)
+public class Complaint_ { 
+
+    public static volatile SingularAttribute<Complaint, String> feedback;
+    public static volatile SingularAttribute<Complaint, String> description;
+    public static volatile SingularAttribute<Complaint, Long> id;
+    public static volatile SingularAttribute<Complaint, String> problemType;
+
+}
